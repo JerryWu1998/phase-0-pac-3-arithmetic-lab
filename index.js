c7f7d38fd4927328220822231341421f31d45c7a
Yu Wu <yw3748@columbia.edu>
2023-05-14 23:06:37 -0400

    hw

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    n++;
+    return n;
+}
+
+function decrement(n) {
+    n--;
+    return n;
+}
+
+function makeInt(n) {
+    const intNumber = parseInt(n, 10);
+    return intNumber;
+}
+
+function preserveDecimal(n) {
+    const floatNumber = parseFloat(n);
+    return floatNumber;
+}
